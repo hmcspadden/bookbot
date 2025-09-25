a2ef3e4f5528fc6602f6b2ae49e32a9c0c5a575a
hmcspadden <hmcspadden26@gmail.com>
2025-09-25 19:34:31 -0400

    added the ability to check different books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count
 from stats import char_count
 from stats import sort_chars
@@ -7,14 +8,20 @@ def get_book_text(filepath):
         book = f.read()
     return book
 
+def has_book():
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 def main():
-    book = get_book_text("./books/frankenstein.txt")
+    has_book()
+    book = get_book_text(sys.argv[1])
     wcount = word_count(book)
     ccount = char_count(book)
     sort_count = sort_chars(ccount)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at /books/frankenstein.txt")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {wcount} total words")
     print("-------- Character Count --------")
